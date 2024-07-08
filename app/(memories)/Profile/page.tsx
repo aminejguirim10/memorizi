6c25856247bb5dbee40776c8435d51ed038bc7c8
Amine Jguirim <aminejguirim10@gmail.com>
2024-07-08 17:31:22 +0100

    :bug: Hopping fixed the named folder profile

--- app/(memories)/Profile/page.tsx
+++ app/(memories)/Profile/page.tsx
@@ -1,6 +1,7 @@
 import MemoriesProfile from "@/components/app/memories-profile";
 import { Metadata } from "next";
 
+
 export const metadata: Metadata = {
   title: "Profile",
   description: "Your profile page.",
